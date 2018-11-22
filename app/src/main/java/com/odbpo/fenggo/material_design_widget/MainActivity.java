1dd4505a60f8f24a45b9d213c96a9a58edde323c
Clearlove2015 <1109623941@qq.com>
2018-11-22 22:38:53 +0800

    折叠标题栏

---
+++ app/src/main/java/com/odbpo/fenggo/material_design_widget/MainActivity.java
@@ -0,0 +1,84 @@
+package com.odbpo.fenggo.material_design_widget;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.odbpo.fenggo.material_design_widget.adapter.MyPagerAdapter;
+import com.odbpo.fenggo.material_design_widget.fragment.IndexFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.viewpager)
+    ViewPager viewpager;
+    @BindView(R.id.tab)
+    TabLayout tab;
+    @BindView(R.id.collapsingToolbarLayout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.tv_setting)
+    TextView tvSetting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setToolbar();
+
+        setViewPager();
+
+    }
+
+    private void setToolbar() {
+        collapsingToolbarLayout.setTitle("导航菜单");
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);
+        collapsingToolbarLayout.setExpandedTitleColor(Color.WHITE);
+    }
+
+    private void setViewPager() {
+        List<Fragment> fragmentList = new ArrayList<>();
+        List<String> titleList = new ArrayList<>();
+        titleList.add("新闻");
+        titleList.add("视频");
+        titleList.add("音乐");
+        titleList.add("军事");
+        titleList.add("体育");
+
+        for (int i = 0; i < titleList.size(); i++) {
+            fragmentList.add(new IndexFragment());
+        }
+
+        viewpager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), fragmentList, titleList));
+        tab.setupWithViewPager(viewpager);
+    }
+
+    @OnClick({R.id.iv_back, R.id.tv_setting})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.tv_setting:
+                Toast.makeText(this,"setting",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/odbpo/fenggo/material_design_widget/fragment/IndexFragment.java
@@ -0,0 +1,21 @@
+package com.odbpo.fenggo.material_design_widget.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.odbpo.fenggo.material_design_widget.R;
+
+public class IndexFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frg_index,container,false);
+        return view;
+    }
+}
